4810a1aae7c84547c56cc2e77c5a3bdb23b3985a
abinpulivelil <67993690+abinpulivelil@users.noreply.github.com>
2023-04-24 20:12:13 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='GeniE_Excel',
+    version='0.1',
+    packages=['GeniE_Excel'],
+    #install_requires=[
+    #    'dependency1',
+    #    'dependency2',
+    #    # Add any other dependencies here
+    #],
+    author='Abin Pulivelil Thomas',
+    author_email='abin.pulivelil@dnv.com',
+    description='This library will import Excel to Convert to GeniE javascript',
+    url='https://github.com/abinpulivelil/GeniE_Excel', 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
